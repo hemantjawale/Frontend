570340542a5308eb58bda718d72a3f2f1712a2db
Hemant Jawale <hemantjawale24@gmail.com>
2025-10-21 16:18:41 +0530

    refactor: remove components from App and integrate router in main

--- reactdom/src/main.jsx
+++ reactdom/src/main.jsx
@@ -2,9 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter, RouterContextProvider, RouterProvider } from 'react-router-dom'
+
+const router = createBrowserRouter([
+{
+  path:'/'
+}
+
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <RouterProvider router={router}/>
     <App />
   </StrictMode>,
 )
