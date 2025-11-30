24fa43e944cfbffcb2ce837472e655411e6f75a0
hemant <hemantjawale24@gmail.com>
2025-11-30 21:17:51 +0530

    feat: implement theme toggle functionality with ThemeProvider and context integration

--- theme_switcher/src/App.jsx
+++ theme_switcher/src/App.jsx
@@ -1,16 +1,37 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
+import { ThemeProvider } from "./Context/Theme.js";
+import { ThemeContext } from "./Context/Theme.js";
+import ThemeBtn from "./Components/ThemeButton.jsx";
+import Card from "./Components/Card.jsx";
 
 function App() {
+  const [themeMode, setThemeMode] = useState("light");
+
+  const lightTheme = () => {
+    setThemeMode("light");
+  };
+
+  const darkTheme = () => {
+    setThemeMode("dark");
+  };
+
+  useEffect(() => {
+    document.querySelector("html").classList.remove("light", "dark");
+    document.querySelector("html").classList.add(themeMode);
+  }, [themeMode]);
+
   return (
-    <ThemeProvider>
+    <ThemeProvider value={{ themeMode, darkTheme, lightTheme }}>
       <div className="flex flex-wrap min-h-screen items-center">
         <div className="w-full">
           <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-            {/* theme button */}
+            <ThemeBtn />
           </div>
 
-          <div className="w-full max-w-sm mx-auto">{/* card   */}</div>
+          <div className="w-full max-w-sm mx-auto">
+            <Card />
+          </div>
         </div>
       </div>
     </ThemeProvider>
