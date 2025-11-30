14bb5cca5d630e595fc526207eb562b1556deb90
hemant <hemantjawale24@gmail.com>
2025-11-30 16:59:56 +0530

    feat: add UserContextProvider component for user state management

---
+++ Context01/src/Context/UserContextProvider.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import usercontext from "./UserContext.js";
+
+const UserContextProvider=({children})=>{
+    
+    const[user,setUser]=useState(null)
+    return(
+        <UserContextProvider value={{user,setUser}}>
+            {children}
+        </UserContextProvider>
+    )
+}
