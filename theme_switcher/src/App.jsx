a9af0e57379b71cea09ecb3fc58ea841c49af0d2
hemant <hemantjawale24@gmail.com>
2025-11-30 20:19:20 +0530

    feat: initialize theme switcher project with React, Tailwind CSS, and Vite
    - Added package.json with dependencies for React, Tailwind CSS, and Vite.
    - Included Vite logo SVG in the public directory.
    - Created App.css and index.css to import Tailwind CSS styles.
    - Developed main App component with basic structure and ThemeProvider context.
    - Implemented Theme context for managing light and dark themes.
    - Added React logo SVG in assets directory.
    - Configured Vite with React and Tailwind CSS plugins in vite.config.js.

---
+++ theme_switcher/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <ThemeProvider>
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+            {/* theme button */}
+          </div>
+
+          <div className="w-full max-w-sm mx-auto">{/* card   */}</div>
+        </div>
+      </div>
+    </ThemeProvider>
+  );
+}
+
+export default App;
