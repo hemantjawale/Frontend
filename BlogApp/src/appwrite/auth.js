70b3727f1c9a3080c7c4f26e5875c607c43cce54
hemant <hemantjawale24@gmail.com>
2025-12-03 19:17:36 +0530

    basaic setup of project and appwrite integration

---
+++ BlogApp/src/appwrite/auth.js
@@ -0,0 +1,18 @@
+import config from "../conf/config.js";
+import { Client, Account, ID } from "appwrite";
+
+export class AuthService {
+  client = new Client();
+  account;
+
+  constructor() {
+    this.client
+      .setEndpoint(config.appwriteUrl)
+      .setProject(config.appwriteProjectId);
+    this.account = new Account(this.client);
+  }
+}
+
+const authservice = new AuthService();
+
+export default authservice;
