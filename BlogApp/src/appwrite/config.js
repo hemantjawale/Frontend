2c95ded9f4db98a92980be2f06fab93782d7a451
hemant <hemantjawale24@gmail.com>
2025-12-12 15:46:34 +0530

    feat: add deletePost, getPost, and getPosts methods in Service class

--- BlogApp/src/appwrite/config.js
+++ BlogApp/src/appwrite/config.js
@@ -48,4 +48,48 @@ export class Service {
       console.log(error);
     }
   }
+
+  async deletePost(slug) {
+    try {
+      await this.databases.deleteDocument(
+        conf.appwriteDatabaseId,
+        conf.appwriteCollectionId,
+        slug
+      );
+      return true;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async getPost(slug){
+    try {
+      return await this.databases.getDocument(
+        conf.appwriteDatabaseId,
+        conf.appwriteCollectionId,
+        slug,
+      )
+    } catch (error) {
+      console.log(error);
+      
+    }
+  }
+
+async getPosts(queries=[Query.equal('status','active')]){
+  try {
+    return await this.databases.listDocuments(
+      conf.appwriteDatabaseId,
+      conf.appwriteCollectionId,
+      queries,
+      100,
+      0
+    )
+  } catch (error) {
+    console.log(error);
+    
+  }
+}
+
+// file upload
+
 }
