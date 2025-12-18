56886f2199b1013b7d70cb04869873a89bb8ce1c
hemant <hemantjawale24@gmail.com>
2025-12-18 11:43:21 +0530

    feat: add EditPost component for post editing with API integration

---
+++ BlogApp/src/Pages/EditPost.jsx
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from "react";
+import { Container } from "../components/Container/Container.jsx";
+import appwriteService from "../appwrite/config.js";
+import { useNavigate, useParams } from "react-router-dom";
+import PostForm from "../components/PostForm/PostForm.jsx";
+
+function EditPost() {
+  const [post, SetPost] = useState(null);
+  const { slug } = useParams();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (slug) {
+      appwriteService.getPost(slug).then((post) => {
+        if (post) {
+          SetPost(post);
+        }
+      });
+    } else {
+      navigate("/");
+    }
+  }, [slug, navigate]);
+
+  return post ? (
+    <div className="py-8">
+      <Container>
+        <PostForm post={post} />
+      </Container>
+    </div>
+  ) : null;
+}
+
+export default EditPost;
